0834a89c43a35792a9d1d2cff4cd48c9884e577c
takegine <405773654@qq.com>
2020-06-14 08:36:27 +0800

    6-14集成

--- scripts/vscripts/skill/PlayerBuild.lua
+++ scripts/vscripts/skill/PlayerBuild.lua
@@ -1,13 +1,15 @@
-function OnBack( data )
+
+skill_player_onback = skill_player_onback or {}
+--------------------------------------------------------------------------------
+
+function skill_player_onback:CastFilterResultTarget(  hTarget ) return ChecktTarget( { hTarget = hTarget, team = self:GetCaster():GetTeamNumber()} ) end
+function skill_player_onback:GetCustomCastErrorTarget(hTarget ) return ErrorTarget( hTarget ) end
+function skill_player_onback:OnSpellStart(  )
     print("PlayerBuild:OnBack")
     
-    local target   = data.target
-    local caster   = data.caster
-
-    if target == caster then return end
-    if not YOUR_IN_TEST and CustomNetTables:GetTableValue( "game_stat", "game_round_stat")["1"]~=0 then return UF_FAIL_CUSTOM end
-
-    local onback   = false
+    local target   = self:GetCursorTarget()
+    local caster   = self:GetCaster()
+   -- local onback   = false
     local iTeam    = target:GetTeamNumber()-5
     local abiName  = "skill_player_bench"
     local tPop     = CustomNetTables:GetTableValue( "Hero_Population", tostring(target:GetPlayerOwnerID())) 
@@ -19,7 +21,8 @@ function OnBack( data )
         target:RemoveAbility(abiName)
         target:RemoveModifierByName("modifier_"..abiName)
         target:SetOrigin(Entities:FindByName(nil,"creep_birth_"..iTeam.."_2"):GetAbsOrigin()+ Vector (RandomFloat(-300, 300),RandomFloat(-100, 200),0) )
-        tPop.popNow = tPop.popNow + target.popuse  
+        target:AddNewModifier(nil, nil, "modifier_phased", {duration=0.1})
+        tPop.popNow = tPop.popNow + target.popuse
         CustomNetTables:SetTableValue( "Hero_Population", tostring(target:GetPlayerOwnerID()),tPop) 
         
         return
@@ -54,47 +57,79 @@ function OnBack( data )
             return
         end
     end
-
-    OnSell( data )
 end
 
-function OnSell( data )
-    local target   = data.target
-    local caster   = data.caster
-    if target == caster then return end
-    if not YOUR_IN_TEST and CustomNetTables:GetTableValue( "game_stat", "game_round_stat")["1"]~=0 then return UF_FAIL_CUSTOM end
+    
+skill_player_onsell = skill_player_onsell or {}
+--------------------------------------------------------------------------------
 
-    local onsell   = false
-    local heroName = target:GetUnitName()
-    local itemName = string.gsub(heroName,"npc","item")
+
+function skill_player_onsell:CastFilterResultTarget(  hTarget ) return ChecktTarget( { hTarget = hTarget, team = self:GetCaster():GetTeamNumber()} ) end
+function skill_player_onsell:GetCustomCastErrorTarget(hTarget ) return ErrorTarget( hTarget ) end
+function skill_player_onsell:OnSpellStart()
+    local target   = self:GetCursorTarget()
+    local caster   = self:GetCaster()
+    local plid     = caster:GetPlayerOwnerID()
+
+    target:ForceKill(true)
+    PlayerResource:Pay( plid, -100 ) 
     
 end
 
-function LevelUp( data )
-    local target   = data.target
-    local caster   = data.caster
-    if target == caster then print("is wrong") return end
-    if not YOUR_IN_TEST and CustomNetTables:GetTableValue( "game_stat", "game_round_stat")["1"]~=0 then return UF_FAIL_CUSTOM end
 
+skill_player_lvlup = skill_player_lvlup or {}
+--------------------------------------------------------------------------------
 
+function skill_player_lvlup:CastFilterResultTarget(  hTarget ) return ChecktTarget( { hTarget = hTarget, team = self:GetCaster():GetTeamNumber()} ) end
+function skill_player_lvlup:GetCustomCastErrorTarget( hTarget ) return ErrorTarget( hTarget ) end
+function skill_player_lvlup:OnSpellStart()
+    local target   = self:GetCursorTarget()
+    local caster   = self:GetCaster()
     local plid     = caster:GetPlayerOwnerID()
     local hero     = PlayerResource:GetSelectedHeroEntity(plid)
     local findcost = 100 --减钱
 
     print(PlayerResource:NumTeamPlayers())
 
-    if  hero:GetGold() > findcost then
-        hero:SetGold(hero:GetGold()-findcost,false)
-        if   target:IsHero() then
-             target:HeroLevelUp(false)
-        else target:CreatureLevelUp(1)
-        end
-        for i=0,10 do 
-            if  target:GetAbilityByIndex(i) then 
-                target:GetAbilityByIndex(i):SetLevel(target:GetLevel()) 
-            end 
-        end 
-    else
-        print("poor guy")
-    end
+    if not PlayerResource:Pay( plid, findcost ) then GetNewHero:UptoDJT(plid,"shopUp","poorguy") 
+        else
+            target:CheckLevel() end
+    -- if  hero:GetGold() > findcost then
+    --     hero:SetGold(hero:GetGold()-findcost,false)
+        
+    -- else
+    --     print("poor guy")
+    -- end
 end
+
+
+--------------------------------------------------------------------------------
+function ChecktTarget(  data )
+    hTarget = data.hTarget
+    team    = data.team
+    if hTarget:GetName()=="npc_dota_hero_phoenix" then return UF_FAIL_COURIER end
+    if CustomNetTables:GetTableValue( "game_stat", "game_round_stat")["1"]~=0 then return UF_FAIL_CUSTOM end
+    return UnitFilter( hTarget, DOTA_UNIT_TARGET_TEAM_BOTH, DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_CREEP, DOTA_UNIT_TARGET_FLAG_MAGIC_IMMUNE_ENEMIES, team )
+end
+
+function ErrorTarget( hTarget )
+    local stat = CustomNetTables:GetTableValue( "game_stat", "game_round_stat")["1"]
+
+    --if self:GetCaster() == hTarget then return "#dota_hud_error_cant_cast_on_self" 
+    if stat==1 then return "#OnGameRoundChange" end
+    if stat==2 then return "#OnGameInProgress"  end
+    -- if hTarget:IsAncient() then
+    --     return "#dota_hud_error_cant_cast_on_ancient"
+    -- end
+
+    -- if hTarget:IsCreep() and ( not self:GetCaster():HasScepter() ) then
+    --     return "#dota_hud_error_cant_cast_on_creep"
+    --end
+    return ""
+end
+
+
+
+
+
+
