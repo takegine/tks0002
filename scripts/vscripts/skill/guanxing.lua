8ffca1d0777ca6728d7af97c42a803545b0df04f
luochengluo123 <9423202@qq.com>
2020-07-11 19:07:38 +0800

    诸葛连弩判断触发双倍观星完成

--- scripts/vscripts/skill/guanxing.lua
+++ scripts/vscripts/skill/guanxing.lua
@@ -1,16 +1,13 @@
-    --首先判断是不是拥有诸葛连弩 如果有，结束这个脚本，如果没有，则删除modifier_skill_hero_guanxing_3修饰器
+    --首先判断是不是拥有诸葛连弩 如果有，则添加modifier_skill_hero_guanxing_3修饰器
     function guanxing(keys)
-        local caster  = keys.caster     --施法者，这里是诸葛
+        local ability  = keys.ability     --技能观星
+        local caster  = keys.caster     --目标，这里是自己本身
     --判断物品：第一个格子（武器栏）是不是诸葛连弩
-    if caster:GetItemInSlot(0) and caster:GetItemInSlot(0):GetName()<'item_weapon_zhugeliannu' then
+    if caster:GetItemInSlot(0) and caster:GetItemInSlot(0):GetName()=='item_weapon_zhugeliannu' then
 
-        caster:RemoveModifierByName("modifier_skill_hero_guanxing_3")
+   ability:ApplyDataDrivenModifier( caster, caster , "modifier_skill_hero_guanxing_3", nil )
 
     end
 
-    --随机1-100，如果大于 触发概率，结束这个脚本
-    if caster:GetItemInSlot(0) and caster:GetItemInSlot(0):GetName()=='item_weapon_zhugeliannu' then 
+end
 
-        return
-        
-     end
