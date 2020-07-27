00feddae5c443f310ea6c49d8cc91df7d448cda6
西索酱 <405773654@qq.com>
2020-07-27 08:08:27 +0800

    修改技能：枭姬

--- scripts/vscripts/skill/hero_xiaoji.lua
+++ scripts/vscripts/skill/hero_xiaoji.lua
@@ -3,7 +3,7 @@
 * @Author: 白喵
 * @Date: 2020-07-24 20:43:01
 * @LastEditors: 白喵
-* @LastEditTime: 2020-07-24 21:44:28
+* @LastEditTime: 2020-07-27 01:32:06
 --]]
 
 
@@ -12,22 +12,18 @@ skill_hero_xiaoji = {}
 function skill_hero_xiaoji:needwaveup()
     local caster = self:GetCaster()
     local owner = caster:GetOwner() or {ship={}}
-    caster:AddNewModifier(caster, self, "modifier_hero_xiaoji", {})
+    caster:AddNewModifier(caster, self, "modifier_hero_xiaoji", nil)
 end
 
-
-
 LinkLuaModifier("modifier_hero_xiaoji", "skill/hero_xiaoji.lua", LUA_MODIFIER_MOTION_NONE)
+
+
 modifier_hero_xiaoji = {}
 
 
-function modifier_hero_xiaoji:DeclareFunctions()
-    return {MODIFIER_PROPERTY_DAMAGEOUTGOING_PERCENTAGE}
-end
 
-function modifier_hero_xiaoji:GetModifierDamageOutgoing_Percentage()
-    local level = self:GetAbility():GetLevel()
-    return 6*level
+function modifier_hero_xiaoji:IsHidden()
+    return true
 end
 
 function modifier_hero_xiaoji:IsAura()
@@ -43,13 +39,13 @@ function modifier_hero_xiaoji:IsDebuff()
 end
 
 function modifier_hero_xiaoji:GetModifierAura()
-	return  "modifier_hero_xiaoji"
+	return  "modifier_hero_xiaoji2"
 end
+
 
 function modifier_hero_xiaoji:GetAuraSearchTeam()
 	return DOTA_UNIT_TARGET_TEAM_FRIENDLY
 end
-
 
 
 function modifier_hero_xiaoji:GetAuraSearchType()
@@ -58,4 +54,20 @@ end
 
 function modifier_hero_xiaoji:GetAuraSearchFlags()
     return DOTA_UNIT_TARGET_FLAG_RANGED_ONLY
+end
+
+LinkLuaModifier("modifier_hero_xiaoji2", "skill/hero_xiaoji.lua", LUA_MODIFIER_MOTION_NONE)
+
+modifier_hero_xiaoji2 = {}
+
+function modifier_hero_xiaoji2:DeclareFunctions()
+    return {MODIFIER_PROPERTY_DAMAGEOUTGOING_PERCENTAGE}
+end
+
+function modifier_hero_xiaoji2:GetModifierDamageOutgoing_Percentage()
+    local level = self:GetAbility():GetLevel()
+    return 6*level
+end
+function modifier_hero_xiaoji2:IsHidden()
+    return false
 end
