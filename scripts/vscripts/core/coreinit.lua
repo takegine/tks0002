f2e328a4dc628afc1d2520a16c3b540c9c6ab492
西索酱 <405773654@qq.com>
2020-07-30 07:56:32 +0800

    微调

--- scripts/vscripts/core/coreinit.lua
+++ scripts/vscripts/core/coreinit.lua
@@ -3,24 +3,24 @@
 item_class = {}
 
 function item_class:lvlup()
-    print( self:GetName())
-    local parent = self:GetParent()
+    -- print( self:GetName())
+    -- local parent = self:GetParent()
     local level  = Clamp(1 + self:GetCurrentCharges(), 0 ,10)
-    print("lvlup", self:GetLevel())
+    -- print("lvlup", self:GetLevel())
     self:SetLevel( level )
 
 end
 
 function item_class:OnUpgrade()
-    local parent = self:GetParent()
+    -- local parent = self:GetParent()
     local level  = self:GetLevel()
     
     self:SetCurrentCharges(level)
 
-    print("OnUpgrade", self:GetLevel())
-    if  string.find(self:GetAbilityName(), 'queue' ) then
-            parent.lvl_queue = level
-    end
+    -- print("OnUpgrade", self:GetLevel())
+    -- if  string.find(self:GetAbilityName(), 'queue' ) then
+    --         parent.lvl_queue = level
+    -- end
 end
 
 function item_class:OnEquip()
@@ -34,4 +34,9 @@ end
 
 function item_class:GetBehavior()
     return DOTA_ABILITY_BEHAVIOR_PASSIVE
+end
+
+
+function item_class:GetIntrinsicModifierName()
+	return "modifier_"..self:GetName()
 end
--- scripts/vscripts/items/5/fengshizhen.lua
+++ scripts/vscripts/items/5/fengshizhen.lua
@@ -2,15 +2,16 @@ item_queue_fengshizhen = item_queue_fengshizhen or class(item_class)
 
 function item_queue_fengshizhen:needwaveup()
     
-    local v = self:GetCaster()
-    local modifierName = "modifier_item_queue_fengshizhen"    
+    local v = self:GetCaster()  
     if  v:GetName() ~= "npc_dota_hero_phoenix" then
-        v:AddNewModifier(v, self, modifierName, {})
-        v:SetModifierStackCount( modifierName, caster ,
+        v:SetModifierStackCount( self:GetIntrinsicModifierName(), caster ,
         v:GetAttackDamage() * self:GetSpecialValueFor("p1")/-100  )
     end
 end
 
+-- function item_queue_fengshizhen:GetIntrinsicModifierName()
+-- 	return "modifier_item_queue_fengshizhen"
+-- end
 ------------------------------------------------------------------
 LinkLuaModifier( "modifier_item_queue_fengshizhen","items/5/fengshizhen", LUA_MODIFIER_MOTION_NONE )
 modifier_item_queue_fengshizhen = modifier_item_queue_fengshizhen or {}
