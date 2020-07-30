9e411264e3a842844a8220bcfc62ccad6186becc
西索酱 <405773654@qq.com>
2020-07-30 18:17:55 +0800

    调整了锋矢阵

--- scripts/vscripts/items/5/fengshizhen.lua
+++ scripts/vscripts/items/5/fengshizhen.lua
@@ -1,17 +1,5 @@
 item_queue_fengshizhen = item_queue_fengshizhen or class(item_class)
 
-function item_queue_fengshizhen:needwaveup()
-    
-    local v = self:GetCaster()  
-    if  v:GetName() ~= "npc_dota_hero_phoenix" then
-        v:SetModifierStackCount( self:GetIntrinsicModifierName(), caster ,
-        v:GetAttackDamage() * self:GetSpecialValueFor("p1")/-100  )
-    end
-end
-
--- function item_queue_fengshizhen:GetIntrinsicModifierName()
--- 	return "modifier_item_queue_fengshizhen"
--- end
 ------------------------------------------------------------------
 LinkLuaModifier( "modifier_item_queue_fengshizhen","items/5/fengshizhen", LUA_MODIFIER_MOTION_NONE )
 modifier_item_queue_fengshizhen = modifier_item_queue_fengshizhen or {}
@@ -21,13 +9,15 @@ function modifier_item_queue_fengshizhen:GetTexture ()
     return "queue/锋矢"
 end
 
-function modifier_item_queue_fengshizhen:DeclareFunctions()	
-    return 
-    { 
-        MODIFIER_PROPERTY_PREATTACK_BONUS_DAMAGE
-    } 
+function modifier_item_queue_fengshizhen:DeclareFunctions()
+    return  
+    {   
+        MODIFIER_PROPERTY_DAMAGEOUTGOING_PERCENTAGE 
+    }
 end
 
-function modifier_item_queue_fengshizhen:GetModifierPreAttack_BonusDamage( params )
-    return -self:GetStackCount() 
+function modifier_item_queue_fengshizhen:GetModifierDamageOutgoing_Percentage()
+    local ability=self:GetAbility()
+    local change = ability:GetSpecialValueFor('p1')
+    return  change
 end
