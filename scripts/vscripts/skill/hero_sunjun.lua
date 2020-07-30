fac7136f6b51d6a30c141b147edc19e4fe516f68
临天屹宇 <546589516@qq.com>
2020-07-30 17:06:09 +0800

    添加技能：孙君

---
+++ scripts/vscripts/skill/hero_sunjun.lua
@@ -0,0 +1,29 @@
+LinkLuaModifier("modifier_skill_hero_sunjun",'skill/hero_sunjun.lua',0)
+
+skill_hero_sunjun=class({})
+
+function skill_hero_sunjun:needwaveup()
+
+    local ability=self
+	local caster=self:GetCaster()
+    local owner =caster:GetOwner() or {ship{}}
+    if owner.ship['junzi'] then
+    caster:AddNewModifier(caster,ability,'modifier_skill_hero_sunjun', {})
+    end
+end
+
+
+modifier_skill_hero_sunjun=class({})   --减免伤害
+
+function modifier_skill_hero_sunjun:DeclareFunctions()
+    return 
+    {
+    MODIFIER_PROPERTY_PHYSICAL_CONSTANT_BLOCK_SPECIAL
+}
+end 
+
+
+function modifier_skill_hero_sunjun:GetModifierPhysical_ConstantBlockSpecial()
+    local ability=self:GetAbility()
+    return  ability:GetLevel()*130
+end
