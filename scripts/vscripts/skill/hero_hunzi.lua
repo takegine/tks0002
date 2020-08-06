313562c50b929586fb644dbb4407f935d3a780c2
临天屹宇 <546589516@qq.com>
2020-08-06 22:17:14 +0800

    添加技能：魂姿

---
+++ scripts/vscripts/skill/hero_hunzi.lua
@@ -0,0 +1,41 @@
+
+LinkLuaModifier( "modifier_skill_hero_hunzi", "skill/hero_hunzi", 0 )
+
+skill_hero_hunzi = class({})
+
+function skill_hero_hunzi:GetIntrinsicModifierName()
+	return "modifier_skill_hero_hunzi"
+end
+
+modifier_skill_hero_hunzi = class({})
+
+function modifier_skill_hero_hunzi:IsHidden()
+    return true
+end
+
+function modifier_skill_hero_hunzi:DeclareFunctions()
+    return
+    {
+        MODIFIER_EVENT_ON_TAKEDAMAGE
+    }
+end 
+
+function modifier_skill_hero_hunzi:OnTakeDamage(keys)
+    local ability = self:GetAbility()
+    local parent  = self:GetParent()
+    local health_perc   = parent:GetHealthPercent()
+  
+    if health_perc <= 75 then
+        local yingzi = parent:AddAbility("skill_hero_yingzi")
+        local yinghun= parent:AddAbility("skill_hero_yinghun")
+        local lvl = ability:GetLevel()
+        yingzi:SetLevel(lvl)
+        yinghun:SetLevel(lvl)
+        yingzi:needwaveup()
+        yinghun:needwaveup()
+        parent:RemoveModifierByName("modifier_skill_hero_hunzi")
+        parent:RemoveAbility("skill_hero_hunzi")
+        
+    end
+end
+
