2ab86244b7ea0acd7b33dc6a2e071cdae2b88ee1
白喵 <478688389@qq.com>
2020-08-14 19:46:10 +0800

    添加曹虎

---
+++ scripts/vscripts/skill/hero_caohu.lua
@@ -0,0 +1,23 @@
+skill_hero_caohu = {}
+
+
+function skill_hero_caohu:needwaveup()
+    local caster = self:GetCaster()
+    local owner = caster:GetOwner() or {ship={}}
+    if owner.ship['huben'] then
+        caster:AddNewModifier(caster, self, "modifier_hero_caohu", nil)
+    end
+end
+
+
+
+
+LinkLuaModifier("modifier_hero_caohu", "skill/hero_caohu.lua", 0)
+modifier_hero_caohu= {}
+function modifier_hero_caohu:DeclareFunctions()
+    return {MODIFIER_PROPERTY_EVASION_CONSTANT}
+end
+
+function modifier_hero_caohu:GetModifierEvasion_Constant()
+    return self:GetAbility():GetSpecialValueFor("chance")
+end
