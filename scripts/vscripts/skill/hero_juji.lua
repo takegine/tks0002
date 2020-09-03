81f08417f9e594b335329be7725ac10d01d87078
表表 <798979502@qq.com>
2020-09-03 22:17:22 +0800

    修改技能 狙击 特效

--- scripts/vscripts/skill/hero_juji.lua
+++ scripts/vscripts/skill/hero_juji.lua
@@ -1,35 +1,31 @@
---   施法
 
---   目标身上增加debuff  减甲 并且获得视野  持续三秒  
-
-
-LinkLuaModifier("skill_hero_juji",'skill/hero_juji.lua',0)
 LinkLuaModifier("modifier_skill_hero_juji",'skill/hero_juji.lua',0)
 
 
 
-skill_hero_juji = class({})   -- 声明一个技能 狙击
+skill_hero_juji = class({})  
 
 function skill_hero_juji:OnSpellStart()
 
-    local caster=self:GetCaster()  --获取施法着
-    local target=self:GetCursorTarget() --获取施法目标
+    local caster=self:GetCaster()  
+    local target=self:GetCursorTarget() 
     local duration=self:GetLevelSpecialValueFor('duration', self:GetLevel()-1)
 
     if target:HasModifier( "modifier_item_sphere_target" ) then
         return
     end
 
-target:AddNewModifier(caster, self, 'modifier_skill_hero_juji', {duration=duration})  -- 添加减甲buff
--- target:AddNewModifier(caster, self, 'modifier_skill_hero_juji2', {duration=duration})  --获得单位视野
+target:AddNewModifier(caster, self, 'modifier_skill_hero_juji', {duration=duration})  
+
+
 
 caster.juji_amountlist = caster.juji_amountlist or {}
 table.insert(caster.juji_amountlist , target)
 
 end
 
-modifier_skill_hero_juji=class({})  --狙击的修饰器  这个修饰器减少护甲5+等级*4
-
+modifier_skill_hero_juji=class({})  
+        
 function modifier_skill_hero_juji:DeclareFunctions()   
   return {
     MODIFIER_PROPERTY_PHYSICAL_ARMOR_BONUS
@@ -41,19 +37,33 @@ function modifier_skill_hero_juji:GetModifierPhysicalArmorBonus()
     return   -(5+caster:GetLevel()*4)
 end
 
--- modifier_skill_hero_juji2=class({})  --  修饰器状态 获得视野
-
--- function modifier_skill_hero_juji2:IsDebuff()
--- 	return  true
--- end
-
--- function modifier_skill_hero_juji2:MODIFIER_STATE_PROVIDES_VISION()
--- 	return  true
--- end
-
 function modifier_skill_hero_juji:CheckState()
     local state = {
-    [MODIFIER_STATE_PROVIDES_VISION]=true,     --MODIFIER_STATE_PROVIDES_VISION
+    [MODIFIER_STATE_PROVIDES_VISION]=true,     
 }      
     return state
 end
+
+
+
+function modifier_skill_hero_juji:GetEffectName()
+    return
+        'particles/units/heroes/hero_slardar/slardar_amp_damage.vpcf'     
+end
+
+function modifier_skill_hero_juji:GetEffectAttachType()
+    return PATTACH_OVERHEAD_FOLLOW
+end
+
+--function modifier_skill_hero_juji:GetOverrideAnimation(params)
+   -- return ACT_DOTA_DISABLED
+--end
+--function modifier_skill_hero_juji:DeclareFunctions()
+  --  local funs={
+ --  MODIFIER_PROPERTY_OVERRIDE_ANIMATION
+ --   }
+--end
+--local pfxname='particles/units/heroes/hero_slardar/slardar_amp_damage.vpcf'
+--local pfx = ParticleManager:CreateParticle( pfxname, PATTACH_OVERHEAD_FOLLOW, target)
+       -- ParticleManager:SetParticleControl(pfx, 0, Vector(n, n, n))
+        --ParticleManager:ReleaseParticleIndex(pfx)
