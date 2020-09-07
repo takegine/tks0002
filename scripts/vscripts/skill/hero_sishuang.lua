57aecd765eaf3971e23d56a3befbeb448575fb34
表表 <798979502@qq.com>
2020-09-07 20:56:07 +0800

    添加技能 司双

---
+++ scripts/vscripts/skill/hero_sishuang.lua
@@ -0,0 +1,87 @@
+
+
+
+LinkLuaModifier("modifier_sishuang_aura",'skill/hero_sishuang.lua',0)
+LinkLuaModifier("modifier_sishuang_buff",'skill/hero_sishuang.lua',0)
+
+skill_hero_sishuang=class({})
+
+function skill_hero_sishuang:GetIntrinsicModifierName()  
+    return "modifier_sishuang_aura"
+end
+
+
+
+modifier_sishuang_aura=class({})
+
+
+function modifier_sishuang_aura:IsAura()
+    return true
+end 
+
+function modifier_sishuang_aura:IsAuraActiveOnDeath()
+    return false 
+end 
+
+function modifier_sishuang_aura:IsDebuff()
+    return false
+end
+
+function modifier_sishuang_aura:GetAuraRadius()
+    return 1500
+end
+
+function modifier_sishuang_aura:GetAuraSearchTeam()
+    return DOTA_UNIT_TARGET_TEAM_FRIENDLY 
+end
+
+function modifier_sishuang_aura:GetAuraSearchType()
+    return DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC
+end 
+
+function modifier_sishuang_aura:GetModifierAura()
+    return 'modifier_sishuang_buff'
+end
+
+function modifier_sishuang_aura:IsHidden()
+    return false
+end
+
+modifier_sishuang_buff=class({})
+
+function modifier_sishuang_buff:IsHidden()
+    return  true
+end
+
+function modifier_sishuang_buff:DeclareFunctions()
+    return{
+        MODIFIER_EVENT_ON_ATTACK  
+    }
+end
+
+function modifier_sishuang_buff:OnAttack(keys)
+    local ability=self:GetAbility()
+    local parent=self:GetParent()
+    local target=keys.attacker
+    local caster=self:GetCaster()
+    local owner =caster:GetOwner() or {ship={}}
+ if not IsServer() then  return  end
+ if keys.target~=parent then return end
+ if owner.ship['sishuang']  then 
+ local dummy = CreateUnitByName( "npc_damage_dummy", Vector(0,0,0), false, parent, parent, parent:GetTeamNumber() )
+ dummy.attack_type  = "fire"
+ dummy:AddNewModifier(dummy, nil, 'modifier_kill', {duration = 0.1} )
+
+ 
+ local  damage_table = {
+    attacker     = dummy,
+    victim       = target,
+    damage_type  = DAMAGE_TYPE_MAGICAL,
+    damage       = target:GetHealth()*3/100,
+    damage_flags = DOTA_DAMAGE_FLAG_NONE
+}
+    ApplyDamage(damage_table)
+else return end
+end
+
+
