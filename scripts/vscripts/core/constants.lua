4c8202bfd504c9c380b101e0c9fad1c333324b9b
西索酱 <405773654@qq.com>
2020-10-15 16:35:13 +0800

    转移常量位置

---
+++ scripts/vscripts/core/constants.lua
@@ -0,0 +1,17 @@
+
+SET_UP_AUTO_LAUNCH_DELAY = 20
+SET_FIRST_HERO     ="minbing"
+MAX_LEVEL          = 10
+SET_PREGAME_TIME   = 0
+TIME_BETWEEN_ROUND = 60
+TIME_BATTER_MAX    = 60
+SET_STARTING_GOLD  = 8000
+LOCAL_POPLATION    = 10
+FOG_OF_WAR_DISABLE = true
+
+SET_FORCE_HERO  = "npc_dota_hero_phoenix"
+tkUnitInfo      = LoadKeyValues('scripts/npc/npc_info_custom.txt')
+tkRounList      = LoadKeyValues('scripts/npc/npc_round_custom.txt')
+DamageKV        = LoadKeyValues("scripts/damage_table.kv")
+
+OUT_SIDE_VECTOR = Vector(8192, -8192, 8192)
--- scripts/vscripts/core/coreinit.lua
+++ scripts/vscripts/core/coreinit.lua
@@ -1,53 +1,53 @@
 
+require("core/constants")
 
-item_class = {}
+item_class = {
+    lvlup = function (self)
+        -- print( self:GetName())
+        -- local parent = self:GetParent()
+        local level  = Clamp(1 + self:GetCurrentCharges(), 0 ,10)
+        -- print("lvlup", self:GetLevel())
+        self:SetLevel( level )
 
-function item_class:lvlup()
-    -- print( self:GetName())
-    -- local parent = self:GetParent()
-    local level  = Clamp(1 + self:GetCurrentCharges(), 0 ,10)
-    -- print("lvlup", self:GetLevel())
-    self:SetLevel( level )
+    end,
+    OnUpgrade = function (self)
+        -- local parent = self:GetParent()
+        local level  = self:GetLevel()
+        
+        self:SetCurrentCharges(level)
 
-end
+        -- print("OnUpgrade", self:GetLevel())
+        -- if  string.find(self:GetAbilityName(), 'queue' ) then
+        --         parent.lvl_queue = level
+        -- end
+    end,
 
-function item_class:OnUpgrade()
-    -- local parent = self:GetParent()
-    local level  = self:GetLevel()
-    
-    self:SetCurrentCharges(level)
-
-    -- print("OnUpgrade", self:GetLevel())
-    -- if  string.find(self:GetAbilityName(), 'queue' ) then
-    --         parent.lvl_queue = level
-    -- end
-end
-
-function item_class:OnEquip()
-    
-    local parent = self:GetParent()
-    local level  = self:GetLevel()
-    
-    print("OnEquip", self:GetLevel())
-end
-
-
-function item_class:GetBehavior()
-    return DOTA_ABILITY_BEHAVIOR_PASSIVE
-end
+    OnEquip = function (self)
+        
+        local parent = self:GetParent()
+        local level  = self:GetLevel()
+        
+        print("OnEquip", self:GetLevel())
+    end,
 
 
-function item_class:GetIntrinsicModifierName()
-    local parent = self:GetParent()
-    local name = "modifier_"..self:GetName()
-    if parent:GetName() == "npc_dota_hero_phoenix" then
-        return name.."_owner"
-    elseif parent:IsHero() then
-        return name.."_hero"
-    else
-        return name.."_unit"
-    end
-end
+    GetBehavior = function (self)
+        return DOTA_ABILITY_BEHAVIOR_PASSIVE
+    end,
+
+
+    GetIntrinsicModifierName = function (self)
+        local parent = self:GetParent()
+        local name = "modifier_"..self:GetName()
+        if parent:GetName() == "npc_dota_hero_phoenix" then
+            return name.."_owner"
+        elseif parent:IsHero() then
+            return name.."_hero"
+        else
+            return name.."_unit"
+        end
+    end,
+}
 
 
 on = function ( self ) return true end
