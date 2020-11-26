56a65b6f145e2616a66ea4aa6ce0bb273177d321
临天屹宇 <546589516@qq.com>
2020-11-26 22:24:08 +0800

    修改技能：红颜

--- scripts/vscripts/skill/hero_hongyan.lua
+++ scripts/vscripts/skill/hero_hongyan.lua
@@ -29,44 +29,46 @@ function modifier_skill_hero_hongyan:DeclareFunctions()
 end 
 
 function modifier_skill_hero_hongyan:OnTakeDamage(keys)
-    local attacker = keys.attacker 
-    local parent  = self:GetParent()
+    if IsServer() then
+        local attacker = keys.attacker 
+        local parent  = self:GetParent()
 
-    if keys.unit == parent and attacker.attack_type == "electrical" then 
-        local caster = self:GetCaster()
-        local ability = self:GetAbility()
-        local damage  = keys.damage *1.575
-        local owner   = caster:XinShi()
-        local healre  =  keys.damage_type == DAMAGE_TYPE_PHYSICAL and damage *2 or damage
+        if keys.unit == parent and attacker.attack_type == "electrical" then 
+            local caster = self:GetCaster()
+            local ability = self:GetAbility()
+            local damage  = keys.damage *1.575
+            local owner   = caster:XinShi()
+            local healre  =  keys.damage_type == DAMAGE_TYPE_PHYSICAL and damage *2 or damage
 
-        if owner.ship["xiaoniao"] then 
-            local healre = healre* 35 /100
-            local radius = 400
-            local target_team     = ability:GetAbilityTargetTeam()
-            local target_types    = ability:GetAbilityTargetType()
-            local target_flags    = ability:GetAbilityTargetFlags() 
-            local units           = FindUnitsInRadius(caster:GetTeamNumber(), 
-                                                      caster:GetOrigin(), 
-                                                      nil, 
-                                                      400,
-                                                      target_team, 
-                                                      target_types, 
-                                                      target_flags, 
-                                                      0, 
-                                                       true)
+            if owner.ship["xiaoniao"] then 
+                local healre = healre* 35 /100
+                local radius = 400
+                local target_team     = ability:GetAbilityTargetTeam()
+                local target_types    = ability:GetAbilityTargetType()
+                local target_flags    = ability:GetAbilityTargetFlags() 
+                local units           = FindUnitsInRadius(caster:GetTeamNumber(), 
+                                                        caster:GetOrigin(), 
+                                                        nil, 
+                                                        400,
+                                                        target_team, 
+                                                        target_types, 
+                                                        target_flags, 
+                                                        0, 
+                                                        true)
 
-            for key,unit in pairs(units) do
-                if unit ~= caster then
-                   unit:Heal(healre, parent)
-                   SendOverheadEventMessage(nil, OVERHEAD_ALERT_HEAL, unit, healre, nil) 
+                for key,unit in pairs(units) do
+                    if unit ~= caster then
+                    unit:Heal(healre, parent)
+                    SendOverheadEventMessage(nil, OVERHEAD_ALERT_HEAL, unit, healre, nil) 
 
-                   local pfx = ParticleManager:CreateParticle("particles/units/heroes/hero_keeper_of_the_light/keeper_of_the_light_chakra_magic.vpcf", PATTACH_POINT_FOLLOW,caster)
-                   ParticleManager:SetParticleControlEnt(pfx, 0,caster, PATTACH_POINT_FOLLOW, "attach_attack1", caster:GetAbsOrigin(), true)
-                   ParticleManager:SetParticleControl(pfx, 1, unit:GetAbsOrigin())
-                   ParticleManager:ReleaseParticleIndex(pfx)
-                end
+                    local pfx = ParticleManager:CreateParticle("particles/units/heroes/hero_keeper_of_the_light/keeper_of_the_light_chakra_magic.vpcf", PATTACH_POINT_FOLLOW,caster)
+                    ParticleManager:SetParticleControlEnt(pfx, 0,caster, PATTACH_POINT_FOLLOW, "attach_attack1", caster:GetAbsOrigin(), true)
+                    ParticleManager:SetParticleControl(pfx, 1, unit:GetAbsOrigin())
+                    ParticleManager:ReleaseParticleIndex(pfx)
+                    end
+                end 
             end 
         end 
-    end 
+    end
 end    
 
